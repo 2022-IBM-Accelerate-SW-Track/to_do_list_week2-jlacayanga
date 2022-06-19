7f4f6bbfbdb7ae6f087f9d7476c5aaa4e4e86b66
Jordyn Lacayanga <jordynlacayanga@Jordyns-MacBook-Pro.local>
2022-06-18 21:12:27 -0400

    first com

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Todos from "../component/todos";
 import AddTodo from "../component/AddTodo";
 import "../pages/Home.css";
+import { breakpoints } from "@mui/system";
 
 class Home extends Component {
   // Create a default state of this component with an empty list of todos.
@@ -19,12 +20,30 @@ class Home extends Component {
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
     // This solution works for a small application but a more complex hashing function should be used when
     // dealing with a larger data sensitive project.
-    todo.id = Math.random();
+    
     // Create a array that contains the current array and the new todo item
+    todo.id = Math.random();
+    if([...this.state.todos].find(t=>t.content===todo.content)){
+      return;
+    }
+    else{
+    
     let new_list = [...this.state.todos, todo];
-    // Update the local state with the new array.
     this.setState({
       todos: new_list,
+    });
+  }
+    
+
+    // Update the local state with the new array.
+    
+  };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos: todos,
     });
   };
   render() {
@@ -36,7 +55,7 @@ class Home extends Component {
         <AddTodo addTodo={this.addTodo} />
         {/* When returning the Todos component, todos is a prop passed to the todos.js file
          to format and render the current todo list state */}
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
